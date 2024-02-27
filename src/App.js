c5da1a31abeb2bd8e8615c9bdbcdf48cfad9119b
CoderKaiHan <hankaicoder@gmail.com>
2024-02-27 10:51:09 -0500

    Implement lazy loading and Suspense

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import * as Loader from 'react-loader-spinner';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+
+const AboutUs = React.lazy(() => import('./components/AboutUs'));
+const Packages = React.lazy(() => import('./components/Packages'));
+
 
 function App() {
   return (
@@ -9,8 +12,18 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<Loader.Oval
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="oval-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
